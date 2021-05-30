f358ffb49aaab1557281b05c35f7e5a346cf1197
leshakrikun <lesha.krikun@users.noreply.github.com>
2021-05-30 15:14:20 +0300

    Update fetchCountries.js

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,32 +1,38 @@
 import '@pnotify/core/dist/BrightTheme.css';
 import countryCardTemplate from './templates/country.hbs'
 import countryCardTemplates from './templates/arrCountry.hbs'
+var debounce = require('lodash.debounce');
 export default query
 
 const { alert, notice, info, success, error } = require('@pnotify/core');
 const refs = {
     cardContainer: document.querySelector('.card-container')
 }
-
+let markup = '';
 function query(countryName) {fetch(`https://restcountries.eu/rest/v2/name/${countryName}`)
 .then(response => {  
     return response.json()
 })
 .then(country  => {
     if (country.length===1){
-        const markup = countryCardTemplate(country)
+        markup = countryCardTemplate(country)
         refs.cardContainer.innerHTML = markup; 
     } else if(country.length<11) {
-        const markup = countryCardTemplates(country)
+        
+        markup = countryCardTemplates(country)
         refs.cardContainer.innerHTML = markup; 
     }
     else {
         refs.cardContainer.innerHTML = '';
-        const myAlert = alert({
-                        text: "Сделайте более специфичный запрос",
+        debounce_fun();
+        document.querySelector('input').setAttribute("disabled", true);
+        alert({
+            text: "Сделайте более специфичный запрос",
             type: 'info'
-            
-          });
+        });
     }
 })
-}  
+}
+const debounce_fun = debounce(function () {
+    document.querySelector('input').removeAttribute("disabled");
+}, 2000);
